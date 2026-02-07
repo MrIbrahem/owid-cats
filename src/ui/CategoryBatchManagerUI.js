4fdc81f40865cd2137c3c696f0466f3d4bae240a
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-07 10:26:52 +0300

    Update CategoryBatchManagerUI.js

--- src/ui/CategoryBatchManagerUI.js
+++ src/ui/CategoryBatchManagerUI.js
@@ -8,7 +8,7 @@
  * @requires OO.ui - MediaWiki's OOUI library for dialogs
  */
 
-/* global APIService, FileService, CategoryService, BatchProcessor, UsageLogger, Validator, OO, SearchHandler, PreviewHandler, ExecuteHandler, ValidationHelper, SearchPanel, FileList */
+/* global APIService, FileService, CategoryService, BatchProcessor, UsageLogger, Validator, OO, SearchHandler, PreviewHandler, ExecuteHandler, ValidationHelper, SearchPanel, FileList, CategoryInputs */
 
 class CategoryBatchManagerUI {
     constructor() {
@@ -19,6 +19,7 @@ class CategoryBatchManagerUI {
 
         // Initialize UI components
         this.searchPanel = new SearchPanel(() => this.searchHandler.handleSearch());
+        this.categoryInputs = new CategoryInputs();
         this.fileList = new FileList(
             () => this.updateSelectedCount(),
             (index) => this.removeFile(index)
